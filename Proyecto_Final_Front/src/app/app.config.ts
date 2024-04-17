0461b7118257d564996a3a4c1b3f3bf5ceeaba1e
Aileen <538aileen@gmail.com>
2024-04-17 17:21:03 +0200

    recuperado

--- Proyecto_Final_Front/src/app/app.config.ts
+++ Proyecto_Final_Front/src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { authInterceptor } from '../../src/app/core/auth.onterceptors';
+import { authInterceptor } from '../app/core/auth.interceptors';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
