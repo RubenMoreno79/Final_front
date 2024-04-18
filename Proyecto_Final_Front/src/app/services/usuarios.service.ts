7308b9a1413148d806f45abab4382e80b0f45137
Ruben_Moreno <rubenm.olivar@gmail.com>
2024-04-18 17:14:16 +0200

    alumnos editar

--- Proyecto_Final_Front/src/app/services/usuarios.service.ts
+++ Proyecto_Final_Front/src/app/services/usuarios.service.ts
@@ -37,37 +37,70 @@ export class UsuariosService {
     }
 
 
-    private baseUrl: string = 'http://localhost:3000/api/usuarios';
+    private baseUrl: string = 'http://localhost:3000/api';
 
     private httpClient = inject(HttpClient);
 
     register(nuevoUsuario: Usuario) {
         return firstValueFrom(
-            this.httpClient.post<RegistroResponse>(`${this.baseUrl}/registro`, nuevoUsuario)
+            this.httpClient.post<RegistroResponse>(`${this.baseUrl}/usuarios/registro`, nuevoUsuario)
         );
     }
 
     login(values: LoginType) {
         return firstValueFrom(
-            this.httpClient.post<LoginResponse>(`${this.baseUrl}/login`, values)
+            this.httpClient.post<LoginResponse>(`${this.baseUrl}/usuarios/login`, values)
         );
     }
+
+    getAlumno() {
+        return firstValueFrom(
+            this.httpClient.get(`${this.baseUrl}/alumnos/alumno`)
+        )
+    }
+
+    editAlumno(userId: number, usuario: any) {
+        usuario.id = userId
+        return firstValueFrom(
+            this.httpClient.put(`${this.baseUrl}/alumnos/editar`, usuario)
+        )
+    }
+
+
+
 
     isLogged() {
         return localStorage.getItem('token_crm') ? true : false;
     }
 
-    isAdmin() {
-        // El role del usuario está codificado dentro del TOKEN
-        const decoded: JwtPayloadCustom = jwtDecode(localStorage.getItem('token_crm')!);
+    // isAdmin() {
+    //     // El role del usuario está codificado dentro del TOKEN
+    //     const decoded: JwtPayloadCustom = jwtDecode(localStorage.getItem('token_crm')!);
 
-        if (decoded.user_role === 'admin') {
-            return true;
-        }
-        return false;
+    //     if (decoded.user_role === 'admin') {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+
+    isusuario() {
+        const decoded: JwtPayloadCustom = jwtDecode(localStorage.getItem('token_crm')!);
+        return decoded
     }
 
 
+    isAlumnoEdit() {
+        const decoded: JwtPayloadCustom = jwtDecode(localStorage.getItem('token_crm')!);
+        console.log(decoded)
+        return decoded
+    }
+
+    // getById(alumnoId: string) {
+    //     return firstValueFrom(
+    //         this.httpClient.get<Usuario>(`${this.baseUrl}/${alumnoId}`)
+    //     )
+    // }
 
 
 
