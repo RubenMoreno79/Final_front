94893e6a709bbae2b9dff88371186f6593f8b5f1
Aileen <538aileen@gmail.com>
2024-04-25 13:37:23 +0200

    home profesor terminada

---
+++ Proyecto_Final_Front/src/app/components/text-profesor/text-profesor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-profesor',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-profesor.component.html',
+  styleUrl: './text-profesor.component.css'
+})
+export class TextProfesorComponent {
+
+}
